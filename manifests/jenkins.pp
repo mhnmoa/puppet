c23e4ef7d8980e843024ddb0c18dc9e7b24b84a8
Mohsen Mohammad Amini <m.mo.amini@gmail.com>
2019-10-14 01:45:09 +0330

    initial commit

---
+++ manifests/jenkins.pp
@@ -0,0 +1,24 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include profile::jenkins
+class profile::jenkins {
+
+  class { 'jenkins': 
+    lts => true,
+    port => 8082,
+    config_hash => {
+      'JENKINS_PORT' => { 'value' => '8082' } ,
+    },
+    configure_firewall => true,
+  }
+
+  package { 'git': 
+    ensure => latest,
+  }
+
+
+
+}
